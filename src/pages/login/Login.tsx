f6167317d49a406a659dfe0c8415e36327732b33
Naah Carvalho <naah_.carvalho@outlook.com>
2025-07-30 11:30:08 -0300

    feat: componente Login com props e formulário

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,26 +2,47 @@ import { useState } from "react";
 import Home from "../home/Home";
 
 function Login() {
+  const [isLogged, setIsLogged] = useState(false);
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
 
-    const [isLogged, setIsLogged] = useState(false);
+  function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
 
-    return (
-        <>
-            {
-                isLogged ? (
-                    <Home
-                        titulo="Componente Home"
-                        texto="Bem-vindo de Volta!"
-                    />
-                ) : (
-                    <div>
-                        <h2>Componente Login</h2>
-                        <button onClick={() => setIsLogged(true)}>Entrar</button>
-                    </div>
-                )
-            }
-        </>
-    )
+    if (usuario === "admin" && senha === "123") {
+      setIsLogged(true);
+    } else {
+      alert("Usuário ou senha inválidos");
+    }
+  }
+
+  return (
+    <>
+      {isLogged ? (
+        <Home titulo="Componente Home" texto={`Bem-vindo de volta!`} />
+      ) : (
+        <form onSubmit={handleLogin}>
+          <h2>Componente Login</h2>
+
+          <input
+            type="text"
+            placeholder="Usuário"
+            value={usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+
+          <button type="submit">Entrar</button>
+        </form>
+      )}
+    </>
+  );
 }
 
-export default Login
+export default Login;
